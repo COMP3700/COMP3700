17703835a5da9abd9884915e0a5930ecf286eb68
Robert Derrow <robbiederrowdev@gmail.com>
2017-11-09 19:28:55 -0600

    adding the updated code for the databse.

--- product_order_database.sql
+++ product_order_database.sql
@@ -4,6 +4,7 @@ CREATE TABLE Products(
     name varchar(255) NOT NULL,
     price decimal(4,2) NOT NULL,
     quantity int NOT NULL,
+    experationDate varchar() NOT NULL,
     providerName varchar(255) NOT NULL,
     providerInfo varchar(255) NOT NULL,
     PRIMARY KEY (productID)
@@ -20,3 +21,27 @@ CREATE TABLE Orders(
     itemPrice decimal(4,2) NOT NULL,
     PRIMARY KEY (orderID)
 );
+
+CREATE TABLE Users (
+    username varchar(255) NOT NULL,
+    password varchar(255) NOT NULL,
+    isAdmin BOOLEAN,
+    profilePic varbinary(255),
+    PRIMARY KEY (username)
+);
+
+/*Code for inserting items into the database*/
+INSERT INTO product (productID, name, price, quantity, experationDate, providerName, providerInfo)
+VALUES (1, "Apple", 0.99, 60, "9/3/2017", "Farmer John", "334-695-4921");
+
+INSERT INTO product (productID, name, price, quantity, experationDate, providerName, providerInfo)
+VALUES (2, "Orange", 1.99, 60, "9/6/2017", "Farmer John", "334-695-4921");
+
+INSERT INTO product (productID, name, price, quantity, experationDate, providerName, providerInfo)
+VALUES (3, "Milk", 2.99, 60, "9/9/2017", "Farmer John", "334-695-4921");
+
+INSERT INTO product (productID, name, price, quantity, experationDate, providerName, providerInfo)
+VALUES (4, "Bacon", 2.99, 60, "9/19/2017", "Farmer John", "334-695-4921");
+
+INSERT INTO product (productID, name, price, quantity, experationDate, providerName, providerInfo)
+VALUES (5, "Cheese", 3.99, 60, "9/20/2017", "Farmer John", "334-695-4921");
